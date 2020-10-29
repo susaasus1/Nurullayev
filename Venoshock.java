db31a8fbebb39f1b9c6d6452ae57e9d685ed3735
susaasus1 <71136245+susaasus1@users.noreply.github.com>
2020-10-29 15:53:24 +0600

    Update Venoshock.java

--- Venoshock.java
+++ Venoshock.java
@@ -5,7 +5,14 @@ public class Venoshock extends SpecialMove {
     public Venoshock(){
         super(Type.POISON,65,100);
     }
+    protected double calcBaseDamage (Pokemon att, Pokemon def) {
+		double d = super.calcBaseDamage(att, def);
+		if (att.getCondition() == Status.POISON)
+			return 2.0 * d;
+		return d;
+	}
     protected String describe() {
         return "Использует способность Venoshock";
     }
+    
 }
